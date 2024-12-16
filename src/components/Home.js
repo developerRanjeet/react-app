694325ce907fe15c24709bee7e406d02478a80da
Ranjeet <stn.ranjeetyadav@gmail.com>
2024-12-16 23:09:25 +0530

    Updated Home function

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
 function Home() {
-    <section>
-        <h3>Home Page</h3>
-    </section>
+    return (
+        <section>
+            <article>
+                <h1>My First Component</h1>
+                <ol>
+                    <li>Components: UI Building Blocks</li>
+                    <li>Defining a Component</li>
+                    <li>Using a Component</li>
+                </ol>
+            </article>
+        </section>
+    )
 }
-
 
 export default Home;
