c4ed9d9b2c5122f55f76098fdaa0c1b4e11565ef
Ranjeet <stn.ranjeetyadav@gmail.com>
2024-12-16 23:09:01 +0530

    Updated Contact function

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 function Contact() {
-    <section>
-        <h3>Contact Page</h3>
-    </section>
+    return (
+        <section>
+            <h3>Contact Page</h3>
+        </section>
+    )
 }
-
 
 export default Contact;
