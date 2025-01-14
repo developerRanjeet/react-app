1a6a3d92ef561b466d65134af6b93f13bb765d6b
Ranjeet Kumar Yadav <stn.ranjeetyadav@gmail.com>
2025-01-14 10:00:05 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-import Home from './components/Home'
-import About from './components/About'
-import Contact from './components/Contact'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className="bg-dark">
-      <Home />
-    </div>
+    <Router>
+      <div className="bg-dark">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
