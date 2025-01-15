7e2d99fbe18813cffcb6ae7cc53bc908b2dde939
Ranjeet Kumar Yadav <stn.ranjeetyadav@gmail.com>
2025-01-15 13:32:59 +0530

    feat: add assets folder

---
+++ public/assets/js/main.js
@@ -0,0 +1,43 @@
+$("#navbarToggler").on("click", function(){
+    $("#navbarNav").slideDown("slow");
+    $("#navbarToggler").hide();
+    $("#navbarCross").show();
+    $(".navbar-brand").hide();
+});
+
+$(document).ready(function() {
+    $("#navbarCross").click(function() {
+        $("#navbarNav").hide();
+        $("#navbarToggler").show();
+        $("#navbarCross").hide();
+        $(".navbar-brand").show();
+    });
+});
+
+
+$(document).ready(function() {
+    let counter = 0;
+    function handleVisibility() {
+        console.log(counter)
+        if ($(window).width() < 995) {
+            if (counter == 0){
+                counter += 1;
+                $("#navbarToggler").show();
+                $("#navbarNav").hide();
+                $("#navbarCross").hide();
+            }  
+        } else {
+            counter = 0
+            $("#navbarNav").show();
+            $("#navbarCross").hide();
+            $(".navbar-brand").show();
+        }
+    }
+    handleVisibility();
+
+    $(window).resize(function() {
+        handleVisibility();
+    });
+});
+
+// Notifications
