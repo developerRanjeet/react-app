c40373def7fab8b0afb84974ef959062cf2ba200
Ranjeet Kumar Yadav <stn.ranjeetyadav@gmail.com>
2025-01-16 12:46:01 +0530

    feat: import components into App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
 
 function App() {
   return (
-    <Router>
-      <div className="bg-dark">
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
-    </Router>
+    <main id='main'>
+      <Header />
+      <Home />
+      <Footer />
+    </main>
   );
 }
 
