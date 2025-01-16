b6b65b86cf883f7f0325d0fcf81b439039732084
Ranjeet Kumar Yadav <stn.ranjeetyadav@gmail.com>
2025-01-16 20:13:53 +0530

    feat: add About section content

--- src/components/About.js
+++ src/components/About.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 function About() {
     return (
-        <section>
-            <h3>About US Page</h3>
-        </section>
+        <div className="about" id="about">
+            <h3 className="text-center mt-5">ABOUT US</h3>
+            <div className="row mt-5 p-2">
+                <div className="col-md-6 testimonial-card mx-auto text-center border rounded shadow">
+                    <p className="mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores fugiat accusamus veniam, possimus maxime rem, sed recusandae veritatis perspiciatis maiores, suscipit quisquam quis voluptate numquam cupiditate fuga debitis est explicabo Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores fugiat accusamus veniam, possimus maxime rem, sed recusandae veritatis perspiciatis maiores, suscipit quisquam quis voluptate numquam cupiditate fuga debitis est explicabo Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores fugiat accusamus veniam, possimus maxime rem, sed recusandae veritatis perspiciatis maiores, suscipit quisquam quis voluptate numquam cupiditate fuga debitis est explicabo </p>
+                    <br/>
+                </div>
+            </div>
+        </div>
     )
 }
 
