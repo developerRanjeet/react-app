3bae7d2e22ab0b805ae3ebb982025eba1fd7a071
Ranjeet Kumar Yadav <stn.ranjeetyadav@gmail.com>
2025-01-16 20:12:51 +0530

    feat: add About, Contact, Services, and Testimonial components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
+import Services from './components/Services';
+import Testimonial from './components/Testimonial';
 import About from './components/About';
 import Contact from './components/Contact';
 
@@ -10,6 +12,10 @@ function App() {
     <main id='main'>
       <Header />
       <Home />
+      <Services />
+      <Testimonial />
+      <About />
+      <Contact />
       <Footer />
     </main>
   );
